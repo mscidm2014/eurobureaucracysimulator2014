a926bcd4b54d0a3e9c09e291d56ed5c4f770b3ed
DonofLizards <katie@Katie.local>
2014-08-14 23:08:16 +0100

    Added comments and removed unnecessary code

--- PythonWebCrawler/tcdcrawler.py
+++ PythonWebCrawler/tcdcrawler.py
@@ -1,49 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
 
-website="http://www.tcd.ie/assets/php/tcd-search/1/proxies/google-search.php?"
+searchList = []
+tidiedSearchList = []
 
+# TCD search URL our search terms will be inserted into
+website="http://www.tcd.ie/assets/php/tcd-search/1/proxies/google-search.php?" 
+
+# Wikipedia URL our search terms are derived from 
 url = "http://en.wikipedia.org/wiki/Glossary_of_business_and_management_terms"
 
+# Get the Wikipedia site, read its contents and create a BeautifulSoup object
 r = requests.get(url)
-
 page = r.text
-
 soup = BeautifulSoup(page)
 
-section = soup.find("span", id = "Verbs").parent
-
-#verbs = section.find_next("ul").find_all("li")
-#verbs = soup.findAll("ul")
-#print(verbs)
-
-verbs = []
-
+# Add all <ul> tags in soup to a list
 uls = soup.findAll("ul")
 
-#print(counter)
-
-#nouns = soup.find_all(id = "Nouns")
-#print(nouns)
-
+#Find all <li> tags in each <ul> tag and append them to the list of search terms
 for ul in uls:
     for li in ul.findAll("li"):
-        verbs = verbs + [li]
+        searchList = searchList + [li]
 
-#nounSection = soup.findAll("h2")[2]
-#nounSection = soup.find("span", id = "Nouns").parent.next_sibling.next_sibling
-#print(nounSection)
 
-#nouns = nounSection.findAll("ul")
-#print(nouns)
-
-#for li in verbs:
-#    print(li)
-
-tidiedVerbs = []
-
-for item in verbs:
+# Purge the text of definitions of terms and other unwanted fluff
+for item in searchList:
         text = item.get_text()
         if "-" in text:
                 dashHead, dashSep, dashTail = text.partition(" -")
@@ -52,20 +34,22 @@ for item in verbs:
                 colonHead, colonSep, colonTail = text.partition(" :")
                 text = dashHead
         elif "<" in item:
-                 #print(text)
                  colonHead, colonSep, colonTail = text.partition("<")
                  text = dashHead
 
 
-        tidiedVerbs = tidiedVerbs + [text]
+        tidiedSearchList = tidiedSearchList + [text]
 
 
-def getSearchResult(verbs):
+def getSearchResult(searchList):
+
+    # Gets the results of a search for each of the search terms on the Wikipedia page
+
     searchTerms = []
-    #print(len(verbs))
-    for item in verbs:
+    for item in searchList:
         searchQuery = item
         
+        # Name/value pairs to be appended to the URL
         postSearch = { "q" : searchQuery,
         "site" : "&sitesearch",
         "start" : 0,
@@ -74,21 +58,20 @@ def getSearchResult(verbs):
         "num" : 10 }
 
         search = requests.get(website, params = postSearch)
-        #search = requests.get(website)
-        #print(search.url)
-        #print(search.url == "http://www.tcd.ie/assets/php/tcd-search/1/proxies/google-search.php?q=hello&site=&sitesearch=&start=0&ie=utf-8&oe=UTF-8&num=10")
-        #print(search.text)
         searchTerms.append(search.text)
 
     return searchTerms
 
 
 def parseSearchResult(search):
+
+    # Creates a soup for each XML search result and appends it to a text file 
+
     counter = 0
     for item in search:
-            soup = BeautifulSoup(item, "xml", from_encoding="utf-8")
+            soup = BeautifulSoup(item, "xml", from_encoding="UTF-8")
             listOfSearchResults = soup.findAll("S")
-            textFile = open("output2.txt", "a")
+            textFile = open("output3.txt", "a")
             textFile.write(tidiedVerbs[counter] + "\n\n")
             counter = counter + 1
 
@@ -102,6 +85,6 @@ def parseSearchResult(search):
     
 
 if __name__ == "__main__":
-    searchResult = getSearchResult(tidiedVerbs)
+    searchResult = getSearchResult(searchList)
     parseSearchResult(searchResult)
     
